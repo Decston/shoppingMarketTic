b76da73f891b2c4fe5cc0e6b1629804bb6ce5695
Alisson Patricio Gomes de Freitas <alissonpatriciodefreitas@gmail.com>
2026-04-14 11:21:26 -0300

    first commit: project vite react ts

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylist-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOption: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["tsconfig.json", "tsconfig.node.json"],
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'react'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "@tyoescript-eslint/no-unsafe-call": "off",
+    "@tyoescript-eslint/no-unsafe-assigment": "off"
+  },
+  setting: {
+    react: {
+        version: "detect",
+    }
+  }
+}
